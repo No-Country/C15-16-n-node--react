31783bc0252d415911bf33b9b3b04d1e24c92cec
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-12-07 09:42:18 -0300

    creacion de rutas login y register con token de autentificacion

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,14 @@
 const { Router } = require("express");
 const hello = require("./hello");
+const login = require("./login");
+const register = require("./register");
 
 //// ENRUTADOR PRINCIPAL ////
 const routes = Router();
 
 //// RUTAS DEFINIDAS (ENTRYPOINT) ////
-routes.use("/", hello)
-
+routes.use("/", hello);
+routes.use("/register", register);
+routes.use("/login", login);
 // EXPORTAR TODAS LAS RUTAS
 module.exports = routes;
