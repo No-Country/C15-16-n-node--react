a1b703a40bede1f6d464738737596303030d96ab
oscar <oscarmunoz03@gmail.com>
2023-12-11 16:17:17 -0500

    add folders

---
+++ client/src/router/index.jsx
@@ -0,0 +1,20 @@
+import React, { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { FullWithLayout } from '../hocs/layouts/FullWithLayout';
+const Home = lazy(() => import('../pages/Home'));
+const SignInSide = lazy(() => import('../pages/SignInSide'));
+const SignUp = lazy(() => import('../pages/SignUp'));
+const NotFound = lazy(() => import('../pages/NotFound'));
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<FullWithLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/SignInSide" element={<SignInSide />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
